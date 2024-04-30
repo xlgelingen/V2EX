03ba9b18a50c52a785665673df56cb22c0aab457
xlgelingen <xuliucg@gmail.com>
2024-04-30 12:14:44 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,20 @@
 // index.js
-Page({})
+Page({
+  data:{
+    topics:[],
+  },
+  onLoad:function(options){
+    this.getData();
+  },
+  getData:function(){
+    wx.request({
+      url: 'https://www.v2ex.com/api/topics/hot.json',
+      success:(res)=>{
+        console.log(res);
+        this.setData({
+          topics: res.data
+        })
+      }
+    })
+  }
+})
