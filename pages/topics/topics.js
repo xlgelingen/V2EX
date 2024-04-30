8f20b4cad7e1c299bc1d74e6527adb148b13b7c4
xlgelingen <xuliucg@gmail.com>
2024-04-30 16:15:55 +0800

    add topics

--- pages/topics/topics.js
+++ pages/topics/topics.js
@@ -1,2 +1,21 @@
 /**topics.js**/
-Page({})
+Page({
+  data:{
+    topics:[],
+  },
+  onLoad:function(options){
+    this.getData(options.id);
+  },
+  getData:function(id){
+    wx.request({
+      url: 'https://www.v2ex.com/api/topics/show.json?node_id='+id,
+      success:(res)=>{
+        console.log('主题页/res',res);
+        this.setData({
+          topics: res.data
+        })
+      }
+    })
+  }
+})
+
