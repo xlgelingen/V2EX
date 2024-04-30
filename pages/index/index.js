068809a2f1e43cad573b0cc559d3ced9bc4c6460
xlgelingen <xuliucg@gmail.com>
2024-04-30 15:30:07 +0800

    add show

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,7 +10,7 @@ Page({
     wx.request({
       url: 'https://www.v2ex.com/api/topics/hot.json',
       success:(res)=>{
-        console.log(res);
+        // console.log(res);
         this.setData({
           topics: res.data
         })
--- pages/show/show.js
+++ pages/show/show.js
@@ -1,2 +1,37 @@
 /**show.js**/
-Page({})
+// index.js
+Page({
+  data:{
+    topic:[],
+    replies:{},
+  },
+  onLoad:function(options){
+    this.getTopics(options.id);
+    this.getReplies(options.id);
+  },
+  getTopics:function(id){
+    wx.request({
+      url: 'https://www.v2ex.com/api/topics/show.json?id='+id,
+      success:(res)=>{
+        // console.log("res",res);
+        // console.log("res.data[0]",res.data[0]);
+        this.setData({
+          topic: res.data[0]
+        })
+      }
+    })
+  },
+  getReplies:function(id){
+    wx.request({
+      url: 'https://www.v2ex.com/api/replies/show.json?topic_id='+id,
+      success:(res)=>{
+        console.log("res",res);
+        // console.log("res.data[0]",res.data[0]);
+        this.setData({
+          replies: res.data
+        })
+      }
+    })
+  }
+})
+
