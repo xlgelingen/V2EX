9c1b15db201773a0069cf12bf89894ce757c3137
xlgelingen <xuliucg@gmail.com>
2024-04-30 16:01:55 +0800

    add node

--- pages/node/node.js
+++ pages/node/node.js
@@ -1,2 +1,21 @@
 /**node.js**/
-Page({})
+Page({
+  data:{
+    nodes:[],
+  },
+  onLoad:function(options){
+    this.getData();
+  },
+  getData:function(){
+    wx.request({
+      url: 'https://www.v2ex.com/api/nodes/all.json',
+      success:(res)=>{
+        console.log('标签/res',res);
+        this.setData({
+          nodes: res.data
+        })
+      }
+    })
+  }
+})
+
